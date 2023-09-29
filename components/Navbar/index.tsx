c104116c2d6b68006498c6ba12eb6adc8f36fd65
민대인 <bamin0422@gmail.com>
2023-09-29 22:58:21 +0900

    add all

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,10 +1,10 @@
 "use client"; // this is a client component
 import { useEffect, useState } from "react";
-import { Link } from "react-scroll";
 import { usePathname } from "next/navigation";
 import { useTheme } from "next-themes";
 import { RiMoonFill, RiSunLine } from "react-icons/ri";
 import { IoMdMenu, IoMdClose } from "react-icons/io";
+import { Link } from "react-scroll/modules";
 
 export interface NavItem {
   label: string;
