7984ef505c018f70c2b2a486f81631ed74c5fcb4
민대인 <bamin0422@gmail.com>
2024-08-02 11:39:18 +0900

    feat: git link 추가

--- components/ProjectCard/index.tsx
+++ components/ProjectCard/index.tsx
@@ -24,7 +24,7 @@ export function ProjectCard({
 }: ProjectCardItem) {
   return (
     <Link
-      href={linkUrl ?? ""}
+      href={linkUrl ?? github ?? ""}
       key={id}
       className="md:h-[174px] h-[138px] dark:bg-slate-950 dark:rounded-2xl rounded-2xl hover:shadow-xl hover:shadow-blue-300 shadow-2xl dark:hover:shadow-blue-700 dark:hover:shadow-xl dark:shadow-blue-900 dark:shadow-xl transition duration-150 ease-out hover:ease-in"
     >
